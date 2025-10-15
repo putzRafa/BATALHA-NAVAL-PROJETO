e6af861114cc009815615d2810be0b8455975336
Rafael Farias de Lima <rafaellimafarias2802@gmail.com>
2025-10-14 22:00:40 -0400

    Correção de Commit e Versao final Tabuleiro

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-void imprime_tabuleiro(){
-    
+void imprime_tabuleiro() {
     int i, j;
     char tabuleiro[10][10];
 
-    for (i = 0; i < 10; i++){
-        for (j = 0; j < 10; j++){
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             tabuleiro[i][j] = '~';
         }
     }
 
-    for (i = 0; i < 10; i++){
-        for (j = 0; j < 10; j++){
-            printf("  %c", tabuleiro[i][j]);
+   printf("      "); 
+    for (char coluna = 'A'; coluna <= 'J'; coluna++) {
+        printf("%c    ", coluna);
+    }
+
+    printf("\n\n");
+    for (i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+
+        for (j = 0; j < 10; j++) {
+            printf("%4c ", tabuleiro[i][j]);
         }
-        printf("\n");
+        printf("\n\n");
     }
 }
+
+
 
 int main (void){
     imprime_tabuleiro();
